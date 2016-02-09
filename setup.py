312517e82bbcdc7c3a58903bfb191cd2fee883b3
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:28:16 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -6,20 +6,26 @@ version = '1.1.2.dev0'
 setup(name='plone.rfc822',
       version=version,
       description="RFC822 marshalling for zope.schema fields",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() +
-                       open(os.path.join("plone", "rfc822", "message.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=(
+          open("README.txt").read() + "\n" +
+          open(os.path.join("docs", "HISTORY.txt")).read() +
+          open(os.path.join("plone", "rfc822", "message.txt")).read()),
+      # Get more strings from
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: BSD License",
-        ],
+          "Framework :: Plone",
+          "Framework :: Plone : 4.3",
+          "Framework :: Plone : 5.0",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: BSD License",
+      ],
       keywords='zope schema rfc822',
       author='Martin Aspeli',
       author_email='optilude@gmail.com',
-      url='http://pypi.python.org/pypi/plone.rfc822',
+      url='https://pypi.python.org/pypi/plone.rfc822',
       license='BSD',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
